472ea3502a8c5fa7dc3277e21f4772516b81880c
blushi <marie.gauthier63@gmail.com>
2018-12-11 14:14:19 +0100

    Added VCO 3 GUI

---
+++ mod-ams.lv2/AMS_VCO3.modgui/modgui/ams-vco3.js
@@ -0,0 +1,10 @@
+function (event) {
+  if (event.type == 'change') {
+    var ledLine = document.getElementById("led-line")
+    if (event.symbol === "waveForm" && (event.value === 2 || event.value === 3)) {
+      ledLine.style.backgroundPosition = "left center";
+    } else {
+      ledLine.style.backgroundPosition = "right center";
+    }
+	}
+}

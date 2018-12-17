8f75090f7512fc3f99991e77a4b932454cfaade9
blushi <marie.gauthier63@gmail.com>
2018-12-17 10:12:50 +0100

    Changed VCO 3 bg and ss images

--- mod-ams.lv2/AMS_VCO3.modgui/modgui/ams-vco3.js
+++ mod-ams.lv2/AMS_VCO3.modgui/modgui/ams-vco3.js
@@ -1,10 +1,12 @@
 function (event) {
   if (event.type == 'change') {
     var ledLine = document.getElementById("led-line")
-    if (event.symbol === "waveForm" && (event.value === 2 || event.value === 3)) {
-      ledLine.style.backgroundPosition = "left center";
-    } else {
-      ledLine.style.backgroundPosition = "right center";
+    if (event.symbol === "waveForm") {
+      if (event.value === 2 || event.value === 3) {
+        ledLine.style.backgroundPosition = "left center";
+      } else {
+        ledLine.style.backgroundPosition = "right center";
+      }
     }
 	}
 }
